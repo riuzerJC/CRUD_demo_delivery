62ef46ea18cbb6a71968ea674270e30e76896166
Ray Cueva <rayjc.desing@gmail.com>
2023-11-12 14:01:55 -0500

    Searching products by id in OrderProductSertvice

--- src/main/java/com/crud/delivery/service/OrderProductService.java
+++ src/main/java/com/crud/delivery/service/OrderProductService.java
@@ -2,6 +2,7 @@ package com.crud.delivery.service;
 
 import com.crud.delivery.entity.Order;
 import com.crud.delivery.entity.OrderProduct;
+import com.crud.delivery.entity.Product;
 import com.crud.delivery.repository.OrderProductRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,21 +10,33 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class OrderProductService {
     @Autowired
     OrderProductRepository orderProductRepository;
+    @Autowired
+    ProductService productService;
 
     public BigDecimal calculateTotalPrice(List<OrderProduct> orderProducts) {
         BigDecimal totalPrice = BigDecimal.ZERO;
 
         for (OrderProduct orderProduct : orderProducts) {
-            BigDecimal productPrice = orderProduct.getProduct().getPriceProduct();
+            Long productId = orderProduct.getProduct().getProductId();
             int quantity = orderProduct.getQuantity();
 
-            BigDecimal productTotalPrice = productPrice.multiply(BigDecimal.valueOf(quantity));
-            totalPrice = totalPrice.add(productTotalPrice);
+            Optional<Product> productOptional = productService.getProducts(productId);
+
+            if (productOptional.isPresent()) {
+                Product product = productOptional.get();
+                BigDecimal productPrice = product.getPriceProduct();
+
+                BigDecimal productTotalPrice = productPrice.multiply(BigDecimal.valueOf(quantity));
+                totalPrice = totalPrice.add(productTotalPrice);
+            } else {
+                System.out.println("Producto no encontrado para el ID: " + productId);
+            }
         }
 
         return totalPrice;
